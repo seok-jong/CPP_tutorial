4f1f5c3e5d7247548d48b334c0ff331acd79fb4e
seok-jong <sjhyeon1222@gmail.com>
2022-08-31 15:38:34 +0900

    초기 커밋.

---
+++ cpp_tutorial/1_2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+// iostream은 c++에서 입출력 관련 헤더파일
+
+int main()
+{
+	std::cout << "Hello" << std::endl;
+	return 0;
+}
---
+++ cpp_tutorial/README.md
@@ -0,0 +1,5 @@
+# CPP Tutorial
+
+학습 자료 :  
+https://modoocode.com/135  
+https://boycoding.tistory.com/135?category=1006674
